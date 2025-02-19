da3dc59470b3b087e097138576a8b4469c9fc58f
FabriceRandrianaivo <fabricerandrianaivo8@gmail.com>
2025-02-19 15:27:28 +0300

    nouveau design portfo en cour : fils portfo.tsx

--- src/pages/portfo.tsx
+++ src/pages/portfo.tsx
@@ -17,18 +17,60 @@ const Item = styled(Paper)(({ theme }) => ({
   }),
 }));
 const projects = [
-  { name: "Project 1", details: "Details about Project 1" },
-  { name: "Project 2", details: "Details about Project 2" },
-  { name: "Project 3", details: "Details about Project 3" },
-  { name: "Project 4", details: "Details about Project 4" },
-  { name: "Project 5", details: "Details about Project 5" },
-  { name: "Project 6", details: "Details about Project 6" },
-  { name: "Project 6", details: "Details about Project 6" },
-  { name: "Project 6", details: "Details about Project 6" },
-  { name: "Project 6", details: "Details about Project 6" },
-  { name: "Project 6", details: "Details about Project 6" },
-  { name: "Project 6", details: "Details about Project 6" },
+  {
+    // year: 2023,
+    name: "myAuxilium",
+    company: "Constellation Group",
+    // progress: "In Progress",
+    technologies: ["React", "TypeScript","Redux", "Bootstrap", "Sass", "MUI", "PostgreSQL", "Python", "FastAPI", "Docker","Open IA"],
+    link: "myAuxilium.ai",
+  },
+  {
+    // year: 2023,
+    name: "Jupiter muAux",
+    company: "Constellation Group",
+    // progress: "In Progress",
+    technologies: ["NextJS","TypeScript" ,"Tailwind", "Rdeux", "Sass",  "PostgreSQL", "Python", "FastAPI", "Docker",],
+    link: "twisales.com",
+  },
+  {
+    // year: 2023,
+    name: "Adherent",
+    company: "Constellation Group",
+    // progress: "Completed",
+    technologies: ["Vue", "Nuxt", "Sass", "TypeScript", "Vuetify", "Strapi", "Nest", "MongoDB"],
+    link: "adherent.ostie.mg",
+  },
+  {
+    // year: 2023,
+    name: "Central Care",
+    company: "Constellation Group",
+    // progress: "In Progress",
+    technologies: ["Vue", "Nuxt", "TypeScript", "Nubra UI", "Sass", "Nest", "MongoDB"],
+    link: "centralcare.ostie.mg",
+  },
+  {
+    // year: 2023,
+    name: "Affilie",
+    company: "Constellation Group",
+    // progress: "Completed",
+    technologies: ["Vue", "Nuxt", "TypeScript", "Sass", "Vuetify", "Strapi", "Nest", "MongoDB"],
+    link: "affilie.ostie.mg",
+  },
 ];
+// const projects = [
+//   { name: "myAuxilium", details: "Details about Project 1" },
+//   { name: "Project 2", details: "Details about Project 2" },
+//   { name: "Project 3", details: "Details about Project 3" },
+//   { name: "Project 4", details: "Details about Project 4" },
+//   { name: "Project 5", details: "Details about Project 5" },
+//   { name: "Project 6", details: "Details about Project 6" },
+//   { name: "Project 6", details: "Details about Project 6" },
+//   { name: "Project 6", details: "Details about Project 6" },
+//   { name: "Project 6", details: "Details about Project 6" },
+//   { name: "Project 6", details: "Details about Project 6" },
+//   { name: "Project 6", details: "Details about Project 6" },
+// ];
 interface headerType {
   theme: boolean;
   // setTheme: (theme: boolean) => void;
@@ -52,7 +94,7 @@ const Portfolio = (props: headerType) => {
         <Box
           sx={{
             display: "flex",
-            flexDirection:"column",
+            flexDirection: "column",
             flexGrow: 1,
             width: selectedProject ? "30%" : "100%",
             transition: "width 0.5s ease",
@@ -68,23 +110,23 @@ const Portfolio = (props: headerType) => {
               "&::-webkit-scrollbar": { display: "none" }, // Cache la barre de défilement sur Webkit (Chrome, Safari, etc.)
               scrollbarWidth: "none", // Cache la barre de défilement sur Firefox
             }}
-            // direction={selectedProject ? "column" : "row"}
+          // direction={selectedProject ? "column" : "row"}
           >
-              {projects.map((project, index) => (
-                <Grid
-                  item
-                  xs={selectedProject ? 12 : 4}
-                  key={index}
-                  onClick={() => handleClick(project.name)}
-                  sx={{
-                    cursor: "pointer",
-                    transition: "transform 0.3s",
-                    "&:hover": { transform: "scale(0.9)" },
-                  }}
-                >
-                  <Item>{project.name}</Item>
-                </Grid>
-              ))}
+            {projects.map((project, index) => (
+              <Grid
+                item
+                xs={selectedProject ? 12 : 4}
+                key={index}
+                onClick={() => handleClick(project.name)}
+                sx={{
+                  cursor: "pointer",
+                  transition: "transform 0.3s",
+                  "&:hover": { transform: "scale(0.9)" },
+                }}
+              >
+                <Item>{project.name}</Item>
+              </Grid>
+            ))}
           </Grid>
         </Box>
 
@@ -95,7 +137,7 @@ const Portfolio = (props: headerType) => {
               width: "70%",
               transition: "width 0.5s ease",
               backgroundColor: "#1a202c",
-              padding: "2rem",
+              padding: "2rem 0",
               display: "flex",
               flexDirection: "column",
               justifyContent: "center",
@@ -103,10 +145,59 @@ const Portfolio = (props: headerType) => {
             }}
           >
             <h2>{selectedProject}</h2>
-            <p>
-              {projects.find((project) => project.name === selectedProject)
-                ?.details || "No details available"}
-            </p>
+            <div className="overflow-x-auto">
+              {/* <table className="min-w-full border-collapse bg-dark text-white text-left"> */}
+
+              <table className="w-auto ">
+                <thead>
+                  <tr className="border-b border-gray-700 text-teal-400">
+                    {/* <th className="p-2">Year</th> */}
+                    {/* <th className="p-2">Project</th> */}
+                    <th className="p-2">Made at</th>
+                    {/* <th className="p-2">Progress</th>  */}
+                    <th className="p-2">Built with</th>
+                    <th className="p-2">Link</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {projects
+                  .filter((project) => project.name === selectedProject)
+                  .map((project, index) => (
+                    <>
+                    <tr key={index} className="border-b border-gray-700">
+                      {/* <td className="p-2">{project.year}</td> */}
+                      {/* <td className="p-2 text-teal-400">{project.name}</td>  */}
+                      <td className="p-2">{project.company}</td>
+                      {/* <td className="p-2">
+                        <span className={`px-2 py-1 rounded ${project.progress === "Completed" ? "bg-green-600" : "bg-blue-600"}`}>
+                          {project.progress}
+                        </span>
+                      </td> */}
+                      <td className="p-2 flex flex-wrap gap-2">
+                        {project.technologies.map((tech, i) => (
+                          <span className="bg-black rounded mx-1 my-0 p-2">
+                            <span key={i} className="bg-black bg-teal-700 px-2 py-3 rounded text-white text-sm">
+                              {tech}
+                            </span>
+                            {i==3 || i==7? <><br/><br/></>: ""}
+                          </span>
+                        ))}
+                      </td>
+                      <td className="p-2">
+                        <a href={`https://${project.link}`} target="_blank" rel="noopener noreferrer" className="text-teal-400 hover:underline">
+                          {project.link}
+                        </a>
+                      </td>
+                    </tr>
+                    <tr>
+                      <td><img src="" alt="" srcset="" /></td>
+                      <td>description</td>
+                    </tr>
+                    </>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </Box>
         )}
       </Box>
